3c90817cdaf83e7a27731074d7f5b51e5aa5f38e
ehdtlaos <eee910413@gmail.com>
2021-08-16 02:51:16 -0700

    k6/http added

---
+++ k6.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  vus: 1000,
+  duration: '10s',
+};
+
+export default function() {
+  http.get(`http://localhost:1004/products/${Math.floor(Math.random() * (1000000 - 1 + 1)) + 1}/related`)
+  sleep(1);
+}
